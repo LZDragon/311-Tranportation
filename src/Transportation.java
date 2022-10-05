2252992d806f3948d14729b4e885417db9eb775f
Eli <elizatelkamp@gmail.com>
2022-10-05 19:59:17 -0400

    Added Transportation.java

---
+++ src/Transportation.java
@@ -0,0 +1,49 @@
+public class Transportation {
+    //////////////////////////
+    //      Variables       //
+    //////////////////////////
+    private double cost;
+    private String purchaseType;
+    private int avgSpeed;
+    private int capacity;
+
+    //////////////////////////
+    //      Constructors    //
+    //////////////////////////
+    Transportation(){
+        this.cost = -1.0;
+        this.purchaseType = "";
+        this.avgSpeed = -1;
+        this.capacity = -1;
+    }
+    Transportation(double cost, String purchaseType, int avgSpeed, int capacity){
+        this.cost = cost;
+        this.purchaseType = purchaseType;
+        this.avgSpeed = avgSpeed;
+        this.capacity =capacity;
+    }
+    ///////////////////////////
+    //        Getters        //
+    ///////////////////////////
+    public double getCost() {return this.cost;}
+    public String getPurchaseType(){return this.purchaseType;}
+    public int getAvgSpeed() {return this.avgSpeed;}
+    public int getCapacity() {return this.capacity;}
+
+    ////////////////////////////
+    //         Setters        //
+    ////////////////////////////
+    public void setCost(double cost) {this.cost = cost;}
+    public void setPurchaseType(String purchaseType) {this.purchaseType = purchaseType;}
+    public void setAvgSpeed(int avgSpeed) {this.avgSpeed = avgSpeed;}
+    public void setCapacity(int capacity) {this.capacity = capacity;}
+
+    @Override
+    public String toString() {
+        return String.format("%n%-15s%-20.2f%n%-15s%-20s%n%-15s%-20d%n%-15s%-20d%n",
+                "Cost",this.cost,
+                "Transaction type",this.purchaseType,
+                "Average Speed",this.avgSpeed,
+                "Capacity",this.capacity);
+    }
+}
